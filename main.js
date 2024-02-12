5e23c4380bd46b6a4d1346fe1a60903d214d727b
Kudinov Ivan <102667861+ikudin0v@users.noreply.github.com>
2024-02-13 00:16:16 +0300

    optimization and adding comments

--- main.js
+++ main.js
@@ -1,36 +1,34 @@
 import { apiRes } from "./api.js"
 
-const openOrHideTree = (item) => {
-	document.getElementById(item.id).hidden = !document.getElementById(item.id).hidden
-	document.getElementById(item.id).hidden === true ? document.getElementById(item.id + "label").textContent = item.name + " (+)":document.getElementById(item.id + "label").textContent = item.name + " (-)"
-}
-
 const getChild = (head) => {
 	let parent
-	head === null ? parent = "root" : parent = head
+	head === null ? parent = "root" : parent = head //указывается родительский элемент перед итерацией рекурсивной функции, если null, то корневой DOM элемент, если нет - id вышележащего узла
 
-	apiRes.services.filter((service) => service.head === head).slice().sort((a,b) => a.sorthead - b.sorthead).map((item) => {
-		let name = document.createElement("div")
-		let service = document.createElement("div")
-		name.id = item.id + "label"
-		service.id = item.id
-		service.style = "margin-left:20px"
-
-		if (item.node === 1) {
-			name.textContent = item.name + " (-)"
-			name.className = "label"
-			document.getElementById(parent).appendChild(name)
-			document.getElementById(parent).appendChild(service)
-			document.getElementById(item.id + "label").addEventListener("click", () => openOrHideTree(item))
-			getChild(item.id)
-		} else {
-			name.textContent = item.name + " (" + item.price + ")"
-			name.className = "item"
-			document.getElementById(parent).appendChild(name)
+	apiRes.services
+	.filter((service) => service.head === head)// данные от api фильтруются по head
+	.slice().sort((a,b) => a.sorthead - b.sorthead) //slice для сортировки копии массива, сортировка по возрастанию по значению ключа sorthead
+	.map((item) => {
+		if (item.node === 1) {//если node - узел(1) то добавляем 2 дочерних div, первый - для имени узла, с id в формате nodeId+"label", второй - контейнер для вложенных элементов с id узла
+			document.getElementById(parent).insertAdjacentHTML("beforeend", `<div id="${item.id+"label"}" class="label">${" (-)" + item.name}</div> 
+																																				<div id="${item.id}" style="margin-left:20px"></div>`)//
+			document.getElementById(item.id + "label").addEventListener("click", () => openOrHideTree(item)) //eventListener на нажатие(вызывает openOrHideTree(), развёртывающую/свёртывающую ветвь дерева)
+			getChild(item.id) //рекурсивно вызываем функцию, но в head уже указывается выше созданный элемент, чтобы найти его детей
+		} else {//если node - лист(0) то добавляем 1 дочерний div, в котором содержится только имя дочернего элемента
+			document.getElementById(parent).insertAdjacentHTML("beforeend", `<div id="${item.id+"label"}" class="item">${item.name + " (" + item.price + ")"}</div>`)
 		}
 	})
 }
 
-getChild(null)
-apiRes.services.filter((service) => service.node === 1).slice().map((item) => openOrHideTree(item))
+const openOrHideTree = (item) => {
+	document.getElementById(item.id).hidden = !document.getElementById(item.id).hidden //свернуть указанную ветвь дерева
+	document.getElementById(item.id).hidden === true //сменить + на - или наоборот в корне ветви дерева, когда она разворачивается или сворачивается
+	? document.getElementById(item.id + "label").textContent = " (+)" + item.name
+	: document.getElementById(item.id + "label").textContent = " (-)" + item.name
+}
+
+getChild(null)//вызов функции для корневых узлов
+
+apiRes.services//свернуть все ветви дерева после построения
+.filter((service) => service.node === 1)
+.map((item) => openOrHideTree(item))
 
